0facebd1d63459c0b849a0815db793494e74839b
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-01-21 18:27:03 -0500

    offset -> axis

--- src/fbp-axis.jl
+++ src/fbp-axis.jl
@@ -1,45 +1,58 @@
 #=
-fbp.jl
-Simple interfaces to FBP for user convenience
+fbp-axis.jl
+Simple interfaces to FBP for AxisArrays for user convenience
+WIP
 =#
 
-using Unitful
-using OffsetArrays: OffsetMatrix, OffsetArray
+#=
+using .AxisArrays: AxisMatrix
+using Sinograms: FBPplan, fbp!
+
+export fbp
+
 
 """
-    fbp(sino::AbstractMatrix{<:Number} ; r, ϕ)
+    fbp(sino::AbstractMatrix{<:Number} ; r, ϕ, args...)
 
 FBP reconstruction from a parallel-beam sinogram of size `[nr × nϕ]`.
 Returns an image of size `[nr × nr]`.
+
+# Options
+* `r` : radial sampling; by default `-(nr-1)/2 : (nr-1)/2`
+* `ϕ` : angular sampling; by default `(0:nϕ-1)/nϕ * π`
 """
 function fbp(
-   sino::AbstractMatrix{<:Number} ;
-   r::StepRangeLen{<:Number} = LinRange(-1,1,size(sino,1)) * (size(sino,1)-1)/2,
-   ϕ::StepRangeLen{<:Number} = (0:size(sino,2)-1) / size(sino,2) * π,
-   kwargs, ...
+    sino::AbstractMatrix{<:Number} ;
+    r::StepRangeLen{<:Number} = LinRange(-1,1,size(sino,1)) * (size(sino,1)-1)/2,
+    ϕ::StepRangeLen{<:Number} = (0:size(sino,2)-1) / size(sino,2) * π,
+    kwargs...,
 )
-   fbp(OffsetArray(sino, r, ϕ) ; kwargs...)
+    fbp(AxisArray(sino; r, ϕ) ; kwargs...)
 end
 
 
+"""
+    fbp(sino::AxisMatrix ; args...)
+
+"""
 function fbp(
-    sino::OffsetMatrix{<:Number} ;
+    sino::AxisMatrix{<:Number} ;
     nx::Int = size(sino, 1),
     ny::Int = ny,
-    dx::RealU = diff(axes(sino)[1][[2,1]]),
+    dx::RealU = diff(axes(sino)[1][[2,1]]), # dr
     dy::RealU = dx,
     x::StepRangeLen{<:Number} = LinRange(-1,1)*(nx-1)/2/nx * dx,
     y::StepRangeLen{<:Number} = LinRange(-1,1)*(ny-1)/2/ny * dy,
 )
     T = promote_type(eltype(sino), Float32)
-    image = OffsetMatrix(zeros(T, nx, ny), x, y)
+    image = AxisMatrix(zeros(T, nx, ny), x, y)
     fbp!(image, sino)
 end
 
 
 function fbp!(
-    image::OffsetMatrix{<:Number},
-    sino::OffsetMatrix{<:Number},
+    image::AxisMatrix{<:Number},
+    sino::AxisMatrix{<:Number},
 )
     sg = sino_geom(:par, sino)
     ig = image_geom(image)
@@ -48,7 +61,7 @@ function fbp!(
 end
 
 
-function sino_geom(how::Symbol, sino::OffsetMatrix)
+function sino_geom(how::Symbol, sino::AxisMatrix)
     sino_geom(how,
         nb = size(sino,1),
         na = size(sino,2),
@@ -58,9 +71,9 @@ function sino_geom(how::Symbol, sino::OffsetMatrix)
         orbit = axes(sino)[2][1]
     )
 end
-       
 
-function image_geom(image::OffsetMatrix)
+
+function image_geom(image::AxisMatrix)
     image_geom(
         nx = size(image,1),
         ny = size(image,2),
@@ -70,3 +83,4 @@ function image_geom(image::OffsetMatrix)
         offset_y = axes(image)[2][1] / dx + nx/2-1
     )
 end
+=#
