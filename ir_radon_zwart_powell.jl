ddbac077385cb27dba0ee3a81f92996ac62c704b
hnanda22 <91295298+hnanda22@users.noreply.github.com>
2022-03-31 15:19:35 -0400

    Updated ir_radon_zwart_powell.jl (#3)

--- ir_radon_zwart_powell.jl
+++ ir_radon_zwart_powell.jl
@@ -1,10 +1,5 @@
-using LazyGrids: ndgrid, ndgrid_array
 using LazyGrids: btime, @timeo # not exported; just for timing tests here
-using BenchmarkTools: @benchmark
 using InteractiveUtils: versioninfo
-using MIRTjim: jim, prompt
-using DelimitedFiles
-using MAT
    # function output = ir_radon_zwart_powell(theta, yr)
    #
    # Compute analytic 2D Radon transform of Zwart-Powell box spline.
@@ -26,27 +21,27 @@ using MAT
    # 2015-08-10 Jeff Fessler, added self test and parallelized
     # if nargin < 2, ir_usage, return, end
 
-function ir_radon_zwart_powell(theta, rr)
+    function ir_radon_zwart_powell(theta, rr)
     dim = size(theta)
-    theta = vec(theta); #converting the matrix theta into a vector with 18281 elements
+    theta = vec(theta) #converting the matrix theta into a vector with 18281 elements
     
-    zeta = zeros(length(theta), 4); #Martrix zeta of dimensions: No. of rows = number of elements in Zeta, No. of coloumns = 4
-    zeta[:,1] = cos.(theta);
-    zeta[:,2] = sin.(theta);
+    zeta = zeros(length(theta), 4) #Martrix zeta of dimensions: No. of rows = number of elements in Zeta, No. of coloumns = 4
+    zeta[:,1] = cos.(theta)
+    zeta[:,2] = sin.(theta)
     #zeta[:,2] = sin.(theta); #in the theta vector the values of above 3.146 gives at for example index 362
     # different values in matlab and julia.. this maybe a probably using the sin function
-    zeta[:,3] .= zeta[:,1] .+ zeta[:,2];
-    zeta[:,4] .= zeta[:,1] .- zeta[:,2]; 
+    zeta[:,3] .= zeta[:,1] .+ zeta[:,2]
+    zeta[:,4] .= zeta[:,1] .- zeta[:,2] 
     #cond = abs.(zeta) .>= eps(1.0)
     cond = abs.(zeta) .>= 1.1920929e-07  #eps('single') matlab different than eps(1.0)- julia
-    N = sum(cond, dims = 2); #returns the sum of the row in the matrix
-    N = vec(N);      
-    output = BoxSp4(rr[:], zeta, cond, N) ./ factorial.(N.-1);
-    output = reshape(output, dim);
+    N = sum(cond, dims = 2) #returns the sum of the row in the matrix
+    N = vec(N)      
+    output = BoxSp4(rr[:], zeta, cond, N) ./ factorial.(N.-1)
+    output = reshape(output, dim)
     return output
     end 
     
-BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
+    BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
 
       #=function output = BoxSp0(y, N)
       %output = heaviside(y) .* y.^(N-1);
@@ -57,9 +52,8 @@ BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
 
     function BoxSp1(y, zeta, cond, N)
       good = cond[:,1]
-      output = (BoxSp0(y .+ (0.5 .* zeta[:,1]), N) .- BoxSp0(y .- (0.5 .* zeta[:,1]), N)) ./ zeta[:,1]
+      output = (BoxSp0.(y .+ (0.5 .* zeta[:,1]), N) .- BoxSp0.(y .- (0.5 .* zeta[:,1]), N)) ./ zeta[:,1]
       output[.~good] .= BoxSp0.(y[.~good], N[.~good])
-
       return output
     end
 
@@ -71,9 +65,9 @@ BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
     end =#
     
     function BoxSp2(y, zeta, cond, N)
-      good = cond[:,2];
-      output = (BoxSp1(y .+ (0.5.*zeta[:,2]), zeta, cond, N) .- BoxSp1(y .- (0.5 .* zeta[:,2]), zeta, cond, N)) ./ zeta[:,2];
-      output[.~good] .= BoxSp1(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good]);
+      good = cond[:,2]
+      output = (BoxSp1(y .+ (0.5.*zeta[:,2]), zeta, cond, N) .- BoxSp1(y .- (0.5 .* zeta[:,2]), zeta, cond, N)) ./ zeta[:,2]
+      output[.~good] .= BoxSp1(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
       return output 
     end
 
@@ -85,10 +79,10 @@ BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
       end =#
     
     function BoxSp3(y, zeta, cond, N)
-      good = cond[:,3];
-      output = (BoxSp2(y .+ 0.5 .* zeta[:,3], zeta, cond, N) .- BoxSp2(y .- 0.5 .* zeta[:,3], zeta, cond, N)) ./ zeta[:,3];
-      output[.~good] .= BoxSp2(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good]);
-      return output;
+      good = cond[:,3]
+      output = (BoxSp2(y .+ 0.5 .* zeta[:,3], zeta, cond, N) .- BoxSp2(y .- 0.5 .* zeta[:,3], zeta, cond, N)) ./ zeta[:,3]
+      output[.~good] .= BoxSp2(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
+      return output
     end
 
     #= function output = BoxSp3(y, zeta, cond, N)
@@ -100,10 +94,10 @@ BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
     end =#
     
     function BoxSp4(y, zeta, cond, N)
-      good = cond[:,4];
+      good = cond[:,4]
       output = (BoxSp3(y .+ 0.5 .* zeta[:,4], zeta, cond, N) .- BoxSp3(y .- 0.5 .* zeta[:,4], zeta, cond, N)) ./ zeta[:,4] 
-      output[.~good] .= BoxSp3(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good]);
-      return output; 
+      output[.~good] .= BoxSp3(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
+      return output
     end
 
     #= MatLab BoxSp4 code:
@@ -113,5 +107,4 @@ BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
         - BoxSp3(y-0.5*zeta(:,4), zeta, cond, N)) ./ zeta(:,4);
       output(~good) = BoxSp3(y(~good), zeta(~good,:), cond(~good,:), N(~good));
       end =#
-
-  #Base.runtests()
+    
--- ir_radon_zwart_powell_test.jl
+++ ir_radon_zwart_powell_test.jl
@@ -1,8 +1,5 @@
-using LazyGrids: ndgrid, ndgrid_array
+using LazyGrids: ndgrid
 using LazyGrids: btime, @timeo # not exported; just for timing tests here
-using BenchmarkTools: @benchmark
-using InteractiveUtils: versioninfo
-using MIRTjim: jim, prompt
 using Test: @test, @testset, @test_throws
 
 @testset "Image reconstruction" begin
@@ -11,6 +8,6 @@ using Test: @test, @testset, @test_throws
     r = LinRange(-1,1,101) * 2
     (tt, rr) = ndgrid(theta, r)
     sino = ir_radon_zwart_powell(tt, rr)
-    #testing if sino is Matrix
+    #testing if sino is a matrix
     @test sino isa Matrix
 end
