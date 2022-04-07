c171ca89fb0c445fbea6fa00dfe30b67e78af671
hnanda22 <91295298+hnanda22@users.noreply.github.com>
2022-04-07 09:26:06 -0400

    ir_radon_zwart_powell.jl

--- ir_radon_zwart_powell_test.jl
+++ ir_radon_zwart_powell_test.jl
@@ -1,9 +1,7 @@
 using LazyGrids: ndgrid
-using LazyGrids: btime, @timeo # not exported; just for timing tests here
 using Test: @test, @testset, @test_throws
 
 @testset "Image reconstruction" begin
-    #defining variables
     theta = LinRange(0,pi,181)
     r = LinRange(-1,1,101) * 2
     (tt, rr) = ndgrid(theta, r)
