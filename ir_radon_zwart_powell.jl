aae61b6520182c165158d26cf47821468a65fc95
hnanda22 <91295298+hnanda22@users.noreply.github.com>
2022-04-13 04:57:40 -0400

    Draft upload

--- ir_radon_zwart_powell.jl
+++ ir_radon_zwart_powell.jl
@@ -18,93 +18,65 @@ IEEE Trans. Med. Imaging, vol. 31, no. 8, pp. 1532–1541, 2012.
 
 =#
 
-
+# ir_radon_zwart_powell
 function ir_radon_zwart_powell(theta, rr)
-  dim = size(theta)
-  theta = vec(theta)    
-  zeta = zeros(length(theta), 4) 
-  zeta[:,1] = cos.(theta)
-  zeta[:,2] = sin.(theta)
-  zeta[:,3] .= zeta[:,1] .+ zeta[:,2]
-  zeta[:,4] .= zeta[:,1] .- zeta[:,2] 
-  cond = abs.(zeta) .>= 1.1920929e-07  #eps('single') matlab different than eps(1.0) in julia
-  N = sum(cond, dims = 2)
-  N = vec(N)      
-  output = BoxSp4(rr[:], zeta, cond, N) ./ factorial.(N.-1)
-  output = reshape(output, dim)
-  return output
-end 
-    
-BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
+    dim = size(theta)
+    theta = vec(theta)    
+    zeta = zeros(length(theta), 4) 
 
-#=
-function output = BoxSp0(y, N)
-  output = heaviside(y) .* y.^(N-1);
-  if any(size(y) ~= size(N)), 
-    keyboard, 
-  end
-  output = (y >= 0) .* y.^(N-1);
-end 
-=#
-    
+    # setting each dimension
+    zeta[:,1] = cos.(theta)
+    zeta[:,2] = sin.(theta)
+    zeta[:,3] .= zeta[:,1] .+ zeta[:,2]
+    zeta[:,4] .= zeta[:,1] .- zeta[:,2]
 
+    # using matlab floating point value of eps(1.0)
+    cond = abs.(zeta) .>= 1.1920929e-07
+
+    N = sum(cond, dims = 2)
+    N = vec(N)      
+    output = BoxSp4(rr[:], zeta, cond, N) ./ factorial.(N.-1)
+    output = reshape(output, dim)
+    return output
+end 
+
+## BoxSp0
+BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)  
+
+## BoxSp1
 function BoxSp1(y, zeta, cond, N)
-  good = cond[:,1]
-  output = (BoxSp0.(y .+ (0.5 .* zeta[:,1]), N) .- BoxSp0.(y .- (0.5 .* zeta[:,1]), N)) ./ zeta[:,1]
-  output[.~good] .= BoxSp0.(y[.~good], N[.~good])
-  return output
+    good = cond[:,1]
+    output = (BoxSp0.(y .+ (0.5 .* zeta[:,1]), N) .- BoxSp0.(y .- (0.5 .* zeta[:,1]), N)) ./ zeta[:,1]
+    output[.~good] .= BoxSp0.(y[.~good], N[.~good])
+    return output
 end
-
-#= 
-function output = BoxSp1(y, zeta, cond, N)
-  good = cond(:,1);
-  output = (BoxSp0(y+0.5*zeta(:,1), N) - BoxSp0(y-0.5*zeta(:,1), N)) ./ zeta(:,1);
-  output(~good) = BoxSp0(y(~good), N(~good));
-end 
-=#
     
+## BoxSp2
 function BoxSp2(y, zeta, cond, N)
-  good = cond[:,2]
-  output = (BoxSp1(y .+ (0.5.*zeta[:,2]), zeta, cond, N) .- BoxSp1(y .- (0.5 .* zeta[:,2]), zeta, cond, N)) ./ zeta[:,2]
-  output[.~good] .= BoxSp1(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
-  return output 
+    good = cond[:,2]
+
+    # Average of second column
+    output = (BoxSp1(y .+ (0.5.*zeta[:,2]), zeta, cond, N) .- BoxSp1(y .- (0.5 .* zeta[:,2]), zeta, cond, N)) ./ zeta[:,2]
+    output[.~good] .= BoxSp1(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
+    return output 
 end
 
-#= 
-function output = BoxSp2(y, zeta, cond, N)
-  good = cond(:,2);
-  output = (BoxSp1(y+0.5*zeta(:,2), zeta, cond, N) - BoxSp1(y-0.5*zeta(:,2), zeta, cond, N)) ./ zeta(:,2);
-  output(~good) = BoxSp1(y(~good), zeta(~good,:), cond(~good,:), N(~good));
-end 
-=#
-    
+## BoxSp3
 function BoxSp3(y, zeta, cond, N)
-  good = cond[:,3]
-  output = (BoxSp2(y .+ 0.5 .* zeta[:,3], zeta, cond, N) .- BoxSp2(y .- 0.5 .* zeta[:,3], zeta, cond, N)) ./ zeta[:,3]
-  output[.~good] .= BoxSp2(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
-  return output
+    good = cond[:,3]
+
+    # Average of third column
+    output = (BoxSp2(y .+ 0.5 .* zeta[:,3], zeta, cond, N) .- BoxSp2(y .- 0.5 .* zeta[:,3], zeta, cond, N)) ./ zeta[:,3]
+    output[.~good] .= BoxSp2(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
+    return output
 end
 
-#=
-function output = BoxSp3(y, zeta, cond, N)
-  good = cond(:,3);
-  output = (BoxSp2(y+0.5*zeta(:,3), zeta, cond, N) - BoxSp2(y-0.5*zeta(:,3), zeta, cond, N)) ./ zeta(:,3);
-  t = zeta(~good,:);
-  output(~good) = BoxSp2(y(~good), t, cond(~good,:), N(~good));
-end 
-=#
-    
+## BoxSp4
 function BoxSp4(y, zeta, cond, N)
-  good = cond[:,4]
-  output = (BoxSp3(y .+ 0.5 .* zeta[:,4], zeta, cond, N) .- BoxSp3(y .- 0.5 .* zeta[:,4], zeta, cond, N)) ./ zeta[:,4] 
-  output[.~good] .= BoxSp3(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
-  return output
-end
+    good = cond[:,4]
 
-#=
-function output = BoxSp4(y, zeta, cond, N)
-  good = cond(:,4);
-  output = (BoxSp3(y+0.5*zeta(:,4), zeta, cond, N) - BoxSp3(y-0.5*zeta(:,4), zeta, cond, N)) ./ zeta(:,4);
-  output(~good) = BoxSp3(y(~good), zeta(~good,:), cond(~good,:), N(~good));
-end =#
-    
+    # Average of fourth column
+    output = (BoxSp3(y .+ 0.5 .* zeta[:,4], zeta, cond, N) .- BoxSp3(y .- 0.5 .* zeta[:,4], zeta, cond, N)) ./ zeta[:,4] 
+    output[.~good] .= BoxSp3(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
+    return output
+end
