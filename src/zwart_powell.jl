8b6e2310d778fd142d8f5ecf1ba622ca9e6d617d
Jeff Fessler <fessler@umich.edu>
2022-04-19 13:57:06 -0400

    use broadcast

--- src/zwart_powell.jl
+++ src/zwart_powell.jl
@@ -6,83 +6,53 @@ Matlab author: Seongjin Yoon, Univ. of Michigan, Jan 2015
 
 export zwart_powell
 
+const TOLERANCE = 1e-07
+
 
 """
-    output = zwart_powell(theta, rr)
-Analytic 2D Radon transform of Zwart-Powell box spline.
-The theta matrix represents the Zwart-Powell element
-by the Box-Spline directions and the distance between ray and the point.
-
-in
-- `theta` : ray angle in radians
-- `rr` : distance between the point and the ray (normalized by the pixel size)
-
-out
-- `output` Radon transform of the Zwart-Powell box spline element
+    output = zwart_powell(r, ϕ)
+Analytic 2D Radon transform value of Zwart-Powell box spline,
+for radial distance `r` (normalized by pixel size) and angle `ϕ` (in radians).
 """
-function zwart_powell(theta, rr)
-    dim = size(theta)
-    theta = vec(theta)
-    zeta = zeros(length(theta), 4)
-
-    # setting each dimension
-    zeta[:,1] = cos.(theta)
-    zeta[:,2] = sin.(theta)
-    zeta[:,3] .= zeta[:,1] .+ zeta[:,2]
-    zeta[:,4] .= zeta[:,1] .- zeta[:,2]
-
-    # using matlab floating point value of eps(1.0)
-    cond = abs.(zeta) .>= 1.1920929e-07
-
-    N = sum(cond, dims = 2)
-    N = vec(N)
-    output = BoxSp4(rr[:], zeta, cond, N) ./ factorial.(N.-1)
-    output = reshape(output, dim)
-    return output
+function zwart_powell(r::Real, ϕ::Real)
+    s, c = sincos(ϕ)
+    return zwart_powell(r, (c, s, c+s, c-s))
 end
 
 
-## BoxSp0
-BoxSp0(y, N::Int) = y > 0 ? y^(N-1) : zero(y)
-
-
-## BoxSp1
-function BoxSp1(y, zeta, cond, N)
-    good = cond[:,1]
-    output = (BoxSp0.(y .+ (0.5 .* zeta[:,1]), N) .- BoxSp0.(y .- (0.5 .* zeta[:,1]), N)) ./ zeta[:,1]
-    output[.~good] .= BoxSp0.(y[.~good], N[.~good])
-    return output
+function zwart_powell(r::Real, z::NTuple{4,T}) where T <: Real
+    N = sum(z -> abs(z) > TOLERANCE, z)
+    return BoxSp4(r, z..., N) / factorial(N-1)
 end
 
 
-## BoxSp2
-function BoxSp2(y, zeta, cond, N)
-    good = cond[:,2]
-
-    # Average of second column
-    output = (BoxSp1(y .+ (0.5.*zeta[:,2]), zeta, cond, N) .- BoxSp1(y .- (0.5 .* zeta[:,2]), zeta, cond, N)) ./ zeta[:,2]
-    output[.~good] .= BoxSp1(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
-    return output
-end
+BoxSp0(y::RealU, N::Int) = y > 0 ? y^(N-1) : zero(y)
 
 
-## BoxSp3
-function BoxSp3(y, zeta, cond, N)
-    good = cond[:,3]
-
-    # Average of third column
-    output = (BoxSp2(y .+ 0.5 .* zeta[:,3], zeta, cond, N) .- BoxSp2(y .- 0.5 .* zeta[:,3], zeta, cond, N)) ./ zeta[:,3]
-    output[.~good] .= BoxSp2(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
-    return output
-end
+BoxSp1(y::RealU, z1::T, N::Int) where T <: Real =
+    abs(z1) > TOLERANCE ?
+        (BoxSp0(y + 0.5 * z1, N) -
+         BoxSp0(y - 0.5 * z1, N)) / z1 :
+        BoxSp0(y, N)
 
 
-## BoxSp4
-function BoxSp4(y, zeta, cond, N)
-    good = cond[:,4]
+BoxSp2(y::RealU, z1::T, z2::T, N::Int) where T <: Real =
+    abs(z2) > TOLERANCE ?
+        (BoxSp1(y + 0.5 * z2, z1, N) -
+         BoxSp1(y - 0.5 * z2, z1, N)) / z2 :
+        BoxSp1(y, z1, N)
 
-    # Average of fourth column
-    output = (BoxSp3(y .+ 0.5 .* zeta[:,4], zeta, cond, N) .- BoxSp3(y .- 0.5 .* zeta[:,4], zeta, cond, N)) ./ zeta[:,4]
-    output[.~good] .= BoxSp3(y[.~good], zeta[.~good,:], cond[.~good,:], N[.~good])
-    return output
+
+BoxSp3(y::RealU, z1::T, z2::T, z3::T, N::Int) where T <: Real =
+    abs(z3) > TOLERANCE ?
+        (BoxSp2(y + 0.5 * z3, z1, z2, N) -
+         BoxSp2(y - 0.5 * z3, z1, z2, N)) / z3 :
+        BoxSp2(y, z1, z2, N)
+
+
+function BoxSp4(y::RealU, z1::T, z2::T, z3::T, z4::T, N::Int) where T <: Real
+    return abs(z4) > TOLERANCE ?
+        (BoxSp3(y + 0.5 * z4, z1, z2, z3, N) -
+         BoxSp3(y - 0.5 * z4, z1, z2, z3, N)) / z4 :
+        BoxSp3(y, z1, z2, z3, N)
 end
