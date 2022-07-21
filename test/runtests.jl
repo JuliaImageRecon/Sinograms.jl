990bb8152397d2df20763739b8d771fea05e5441
Jeff Fessler <JeffFessler@users.noreply.github.com>
2022-07-21 16:52:58 -0400

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,15 +5,15 @@ using Test: @test, @testset, detect_ambiguities
 
 include("helper.jl")
 
+# todo: more
+
+include("sino-geom.jl")
+include("fbp/window.jl")
+include("fbp2/ramp.jl")
+include("fbp-plan.jl")
+include("fbp-par.jl")
+include("zwart_powell.jl")
+
 @testset "Sinograms" begin
-    # todo: more
-
-    include("sino-geom.jl")
-    include("fbp/window.jl")
-    include("fbp2/ramp.jl")
-    include("fbp-plan.jl")
-    include("fbp-par.jl")
-    include("zwart_powell.jl")
-
-    @test length(detect_ambiguities(Sinograms)) == 0
+    @test isempty(detect_ambiguities(Sinograms))
 end
