83bf446507c8f93f999c2627314eacd1870fd038
Jeff Fessler <fessler@umich.edu>
2022-07-26 19:51:16 -0400

    infer fbp

--- test/fbp-fan.jl
+++ test/fbp-fan.jl
@@ -4,27 +4,37 @@ Test fan-beam FBP
 =#
 
 using Sinograms: plan_fbp, fbp, SinoFanArc, SinoFanFlat, rays
-using ImageGeoms: ImageGeom
+using ImageGeoms: ImageGeom, MaskCircle, circle
 using Test: @test, @testset, @inferred
-#using MIRTjim: jim; jim(:prompt, true) # debug only
+#using MIRTjim: jim; jim(:prompt, true)
 
 include("helper.jl")
 
 @testset "fbp-fan" begin
-    ig = ImageGeom()
     proj1 = r -> abs(r) < 1 ? 2 * sqrt(1 - r^2) : 0. # unit disk sinogram
     proj2 = (r, ϕ, x, y) -> proj1(r - (x * cos(ϕ) + y * sin(ϕ))) # shifted
-    for sg in (SinoFanArc(), SinoFanFlat())
+    for geom in (SinoFanArc, SinoFanFlat)
+        sg = geom( ; d = 0.1f0)
+        ig = ImageGeom(MaskCircle() ; deltas = (1,1) .* 0.1f0)
+        mask = circle(ig ; r = sg.rfov)
+        ig = ImageGeom(ig.dims, ig.deltas, ig.offsets, mask)
         r, ϕ = rays(sg)
         sino = proj2.(r, ϕ, 3, 1)
-#       jim(r, ϕ, sino)
+#       jim(sg.s, sg.ad, sino)
 
         plan = @inferred plan_fbp(sg, ig)
-        recon, sino_filt = @NOTinferred fbp(plan, sino) # todo
+
+        recon, sino_filt = @NOTinferred fbp(plan, sino)
         @test recon isa Matrix
-#       jim(recon) # todo: peak is too low by about "dr"
+#       jim(axes(ig), recon)
+
+        area_hat = sum(recon) * prod(ig.deltas)
+        area_true = π * 1^2
+        area_err = abs(area_hat - area_true) / area_true
+        @test area_err < 2e-4
+
         sino3 = reshape(sino, size(sino)..., 1)
-        recon3 = @NOTinferred fbp(plan, sino3) # todo
+        recon3 = @NOTinferred fbp(plan, sino3)
         @test recon3[:,:,1] == recon
     end
 end
--- test/fbp-par.jl
+++ test/fbp-par.jl
@@ -1,12 +1,11 @@
 #=
-fbp-par.jl
+test/fbp-par.jl
 Test basic parallel-beam FBP
 =#
 
 using Sinograms: fbp
 using Test: @test, @testset, @inferred
 #using MIRTjim: jim; jim(:prompt, true) # debug only
-
 
 @testset "fbp-par" begin
     nr = 64
@@ -20,7 +19,12 @@ using Test: @test, @testset, @inferred
     sino = proj2.(r, ϕ', 3, 1)
 #   jim(r, ϕ, sino)
 
-    recon = @inferred fbp(sino)
+    recon = @inferred fbp(sino; dr)
     @test recon isa Matrix
-#   jim(recon) # todo: peak is too low by about "dr"
+#   jim(recon; prompt=false, gui=true)
+
+    area_hat = sum(recon) * dr^2
+    area_true = π * 1^2
+    area_err = abs(area_hat - area_true) / area_true
+    @test area_err < 1e-3
 end
