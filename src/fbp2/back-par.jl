b873b2f5afc2c26fd3f21591e7f5287328fa0d08
Jeff Fessler <fessler@umich.edu>
2022-09-23 11:17:19 +0200

    pixel-driven back par

--- src/fbp2/back-par.jl
+++ src/fbp2/back-par.jl
@@ -13,15 +13,15 @@ using ImageGeoms: ImageGeom, embed
 
 2D pixel-driven backprojection for FBP.
 
-in
+# in
 - `sg::SinoGeom`
 - `ig::ImageGeom`
 - `sino::AbstractArray{<:Number}` sinogram(s) (line integrals), usually ramp filtered
 
-options
+# options
 - `ia_skip::Int` downsample in angle to save time for quick tests (default: 1)
 
-out
+# out
 - `img::Array{<:Number}` reconstructed image(s)
 """
 fbp_back
@@ -44,23 +44,27 @@ function fbp_back(
     return fbp_back_par(
         sino, sg.ar,
         sg.ds, sg.offset,
-        sg.rfov,
-        ndgrid(axes(ig)...)...,
-        ig.mask, ia_skip,
+#       sg.rfov,
+#       ndgrid(axes(ig)...)...,
+        axes(ig)...,
+        ig.mask ; ia_skip,
     )::Matrix{T}
 end
 
 
-function fbp_back_par(
+#=
+# old "matlab-like" way with lots of broadcast
+function fbp_back_par_old(
     sino::AbstractMatrix{Ts},
     angles::AbstractVector{To},
     ds::Tds,
     offset::Toffset,
-    rfov::RealU,
+#   rfov::RealU,
     xc::AbstractArray{Tc},
     yc::AbstractArray{Tc},
-    mask::AbstractMatrix{Bool},
-    ia_skip::Int,
+    mask::AbstractMatrix{Bool} ;
+    ia_skip::Int = 1,
+    warned::Bool = false,
 ) where {Tds <: RealU, Tc <: RealU, Toffset <: Real, Ts <: Number, To <: RealU}
 
     Td = promote_type(Tds, Tc)
@@ -85,12 +89,11 @@ function fbp_back_par(
 
     img = zero(T)
 
-    warned = false
     for ia in 1:ia_skip:na
 
         angle = angles[ia]
-        (sang, cang) = sincos(angle)
-        bb = @. (xc * cang + yc * sang) # [np]
+        (sinϕ, cosϕ) = sincos(angle)
+        bb = @. (xc * cosϕ + yc * sinϕ) # [np]
         bb = @. (bb / ds + wb) # unitless bin index
 
 #=
@@ -125,88 +128,181 @@ function fbp_back_par(
     img .*= (π * ia_skip / na)
     return embed(img, mask)::Matrix{T}
 end
+=#
 
 
-function fbp_back_par!(
-    image::AbstractMatrix{T},
+# new version with threads and simplified code
+
+"""
+    fbp_back_par(sino, angles, ds, offset, xc, yc, mask, ia_skip; T)
+Pixel-driven back-projection
+for a grid of `(xc,yc)` pixel center locations
+for sinogram `sino` from a parallel-beam geometry.
+It assumes the angles are equally spaced over `[0,π)`.
+
+# in
+- `sino::Matrix{<:Number}` `(nb, na)` usually ramp-filtered
+- `angles::Vector{<:Real}` `(na)` in radians
+- `ds::RealU` ray spacing
+- `offset::Real` detector offset (usually 0)
+- `xc::Vector{<:RealU}` `(nx)` pixel centers
+- `yc::Vector{<:RealU}` `(ny)` pixel centers
+- `mask::Matrix{Bool}` `(nx, ny)` which pixels to reconstruct
+
+# option
+- `ia_skip::Int` default 1
+- `T::DataType` usually same as `eltype(sino)`
+
+# out
+- `image::Matrix` `(nx, ny)` matrix to be mutated
+"""
+function fbp_back_par(
     sino::AbstractMatrix{Ts},
     angles::AbstractVector{To},
     ds::Tds,
     offset::Toffset,
-    rfov::RealU,
     xc::AbstractArray{Tc},
     yc::AbstractArray{Tc},
-    mask::AbstractMatrix{Bool},
-    ia_skip::Int,
-) where {Tds <: RealU, Tc <: RealU, Toffset <: Real, Ts <: Number, To <: RealU}
+    mask::AbstractMatrix{Bool} ;
+    ia_skip::Int = 1,
+    T = eltype(oneunit(Ts) * (oneunit(To) * oneunit(Tc) / oneunit(Tds) + oneunit(Toffset)))
+) where {Ts <: Number, To <: RealU, Tds <: RealU, Toffset <: Real, Tc <: RealU}
 
-# todo: some interface
-#   Td = promote_type(Tds, Tc)
-#   T = eltype(oneunit(Ts) * (oneunit(Td) * oneunit(To) / oneunit(Td) + oneunit(Toffset)))
+    image = zeros(T, size(mask)) # need zero(T) outside mask
+    sinϕ = sin.(angles[1:ia_skip:end])
+    cosϕ = cos.(angles[1:ia_skip:end])
+    fbp_back_par!(image, sino, sinϕ, cosϕ, ds, offset, xc, yc, mask ; ia_skip)
+    return image
+end
+
+
+"""
+    fbp_back_par!(image, sino, sinϕ, cosϕ, ds, offset, xc, yc, mask, ia_skip)
+Mutating version of
+pixel-driven back-projection
+for a grid of `(xc,yc)` pixel center locations
+for sinogram `sino` from a parallel-beam geometry.
+It uses `Threads`.
+It assumes the angles are equally spaced over `[0,π)`.
+
+# in
+- `sino::Matrix{<:Number}` `(nb, na)` usually ramp-filtered
+- `sinϕ::Vector{<:Real}` `(na)`
+- `cosϕ::Vector{<:Real}` `(na)`
+- `ds::RealU` ray spacing
+- `offset::Real` detector offset (usually 0)
+- `xc::Vector{<:RealU}` `(nx)` pixel centers
+- `yc::Vector{<:RealU}` `(ny)` pixel centers
+- `mask::Matrix{Bool}` `(nx, ny)` which pixels to reconstruct
+
+# option
+- `ia_skip::Int` default 1
+
+# out
+- `image::Matrix` `(nx, ny)` matrix to be mutated
+"""
+function fbp_back_par!(
+    image::AbstractMatrix{T},
+    sino::AbstractMatrix{<:Number},
+    sinϕ::AbstractVector{<:Real}, # sin.(angles) (na_subset)
+    cosϕ::AbstractVector{<:Real}, # cos.(angles) (na_subset)
+    ds::RealU,
+    offset::Toffset,
+    xc::AbstractArray{<:RealU},
+    yc::AbstractArray{<:RealU},
+    mask::AbstractMatrix{Bool} ;
+    ia_skip::Int = 1,
+) where {T <: Number, Toffset <: Real}
 
     length.((xc,yc)) == size(image) == size(mask) || throw("size mismatch")
 
     nb, na = size(sino)
 
     wb = Toffset((nb+1)/2 + offset)
-    sang = sin.(angles[1:ia_skip:end])
-    cang = cos.(angles[1:ia_skip:end])
-    sino = @view sino[:,1:ia_skip:end]
+    if ia_skip > 1
+        sino = @view sino[:,1:ia_skip:end]
+    end
+    xc_ds = xc / ds
+    yc_ds = yc / ds
 
-# todo threads
-    for c in findall(mask)
+    image[.! mask] .= zero(T)
+
+    Threads.@threads for c in findall(mask)
         image[c] = fbp_back_par_xy(
-            sino, sang, cang, ds, wb, rfov,
-            xc[c[1]], yc[c[2]]; T,
+            sino, sinϕ, cosϕ, wb,
+            xc_ds[c[1]], yc_ds[c[2]]; T,
         )
     end
+
+#=
+    # similar speed
+    for iy in 1:size(mask,2), ix in 1:size(mask,1)
+        if mask[ix,iy]
+            image[ix,iy] = fbp_back_par_xy(
+                sino, sinϕ, cosϕ, wb,
+                xc_ds[ix], yc_ds[iy]; T,
+            )
+        end
+    end
+=#
 
     return image
 end
 
 
 """
-    fbp_back_par_xy(...)
+    fbp_back_par_xy(sino, sinϕ, cosϕ, wb, x, y ; T)
 Pixel-driven back-projection for a single (x,y) location
+for sinogram `sino` from a parallel-beam geometry.
+It assumes the angles are equally spaced over `[0,π)`.
+
+# in
+- `sino::Matrix{<:Number}` `(nb, na)` usually ramp-filtered
+- `sinϕ::Vector{<:Real}` `(na)`
+- `cosϕ::Vector{<:Real}` `(na)`
+- `wb::Real = (nb+1)/2 + offset` where usually `offset=0`
+- `x,y::Real` pixel center location, normalized by ray spacing
+
+# option
+- `T::DataType` typically same as `eltype(sino)`
+
+# out
+- Returns a scalar of type `T`.
 """
 function fbp_back_par_xy(
     sino::AbstractMatrix{Ts}, # (nb, na_subset)
-    sang::AbstractVector{To}, # sin.(angles) (na_subset)
-    cang::AbstractVector{To}, # cos.(angles) (na_subset)
-    ds::Td,
+    sinϕ::AbstractVector{To}, # sin.(angles) (na_subset)
+    cosϕ::AbstractVector{To}, # cos.(angles) (na_subset)
     wb::Tb, # (nb+1)/2 + offset
-    rfov::RealU,
-    x::Td,
-    y::Td ;
-    T::DataType = eltype(oneunit(Ts) * (oneunit(Td) * oneunit(To) / oneunit(Td) + oneunit(Tb))),
-) where {Td <: RealU, Tb <: Real, Ts <: Number, To <: RealU}
+    x_ds::Tx, # xc / ds
+    y_ds::Tx ;
+    T::DataType = eltype(oneunit(Ts) * one(To) * one(Tb) * one(Tx)),
+) where {Ts <: Number, To <: Real, Tb <: Real, Tx <: Real}
 
-    nb, na_subset = size(sino)
+    nb = size(sino,1)
+    na_subset = size(sino,2)
 
     pixel = zero(T)
 
     for ia in 1:na_subset
-        bb = x * cang[ia] + y * sang[ia]
-        bb = bb / ds + wb # unitless bin index
+        @inbounds bb = x_ds * cosϕ[ia] + y_ds * sinϕ[ia] + wb # unitless bin index
 
 #=
         # nearest neighbor interpolation:
-        ib = round.(Int, bb)
-        # trick: make out-of-sinogram indices point to those extra zeros
-        @. ib[ib < 1 | ib > nb] = nb+1
-        img += sino[ib, ia]
+        ib = round(Int, bb)
+        if 1 ≤ ib ≤ nb
+            @inbounds pixel += sino[ib, ia]
+        end
 =#
 
         # linear interpolation:
         il = floor(Int, bb) # left bin
         ir = 1 + il # right bin
 
-        if (il ≥ 1) & (ir ≤ nb)
-
+        if (il ≥ 1) && (ir ≤ nb)
             wr = bb - il # left weight
             wl = 1 - wr # right weight
-
-            pixel += wl * sino[il, ia] + wr * sino[ir, ia]
+            @inbounds pixel += wl * sino[il, ia] + wr * sino[ir, ia]
         end
     end
 
--- test/fbp2/back.jl
+++ test/fbp2/back.jl
@@ -1,9 +1,56 @@
 # test/fbp2/back.jl
 
-using ImageGeoms: ImageGeom
+using ImageGeoms: ImageGeom, MaskCircle
+using LazyGrids: ndgrid
+using Random: seed!
 using Sinograms: SinoPar, SinoFanArc, SinoFanFlat, fbp_back
+using Sinograms: fbp_back_par, fbp_back_par!, fbp_back_par_xy
 using Unitful: mm
 using Test: @test, @testset, @inferred
+using BenchmarkTools
+
+#=
+# verify new pixel-driven vs old broadcasting/allocating way
+
+using Sinograms: fbp_back_par_old
+
+#   ig = @inferred ImageGeom(MaskCircle(), dims=(32,30), deltas=(1mm,1mm) )
+    ig = @inferred ImageGeom(dims=(32,30), deltas=(1mm,1mm) )
+#   sg = SinoPar( ; nb = 12, d=2mm) # intentionally small FOV
+    sg = SinoPar( ; nb = 36, d=1mm)
+
+    seed!(0)
+    xc, yc = axes(ig)
+    sino = randn(Float32, sg.dim)
+    f1 = sino -> fbp_back_par_old(sino, sg.ar, sg.ds, sg.offset, # sg.rfov,
+         ndgrid(xc, yc)..., ig.mask ; warned=true)
+    f2 = sino -> fbp_back_par(sino, sg.ar, sg.ds, sg.offset,
+         xc, yc, ig.mask)
+    sang = sin.(sg.ar)
+    cang = cos.(sg.ar)
+    f3! = (image, sino) -> fbp_back_par!(image, sino, sang, cang,
+         sg.ds, sg.offset, xc, yc, ig.mask)
+    b1 = f1(sino)
+    b2 = f2(sino)
+    b3 = zeros(eltype(b1), size(b1))
+    f3!(b3, sino)
+    @test b1 ≈ b2 ≈ b3
+
+#=
+    x = xc[10] / sg.d
+    y = yc[10] / sg.d
+    f4 = sino -> fbp_back_par_xy(sino, sang, cang, sg.w,
+          x, y ; T = Float32)
+    f4(sino); # warm up
+
+    @btime f4($sino);
+=#
+
+    @btime f1($sino);
+    @btime f2($sino);
+    @btime f3!($b3, $sino);
+=#
+
 
 @testset "fbp2/back" begin
     ig = @inferred ImageGeom( dims=(32,30), deltas=(1mm,1mm) )
