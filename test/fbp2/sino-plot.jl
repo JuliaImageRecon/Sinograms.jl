df3c446fc13e78ce1dc3e809a9fda8a554adf218
Jeff Fessler <fessler@umich.edu>
2022-10-01 22:34:34 +0200

    refactor sino_plot

--- test/fbp2/sino-plot.jl
+++ test/fbp2/sino-plot.jl
@@ -1,44 +1,37 @@
 # test/fbp2/sino-plot.jl
 
-# include("helper.jl")
-
 using Plots: plot, gui, Plot
 using Unitful: mm #, °
-using UnitfulRecipes
-#using MIRTjim: prompt
-using Sinograms: SinoPar, SinoMoj, SinoFanArc, SinoFanFlat
+using Sinograms: SinoPar, SinoMoj, SinoFanArc, SinoFanFlat, SinoFan
 using Sinograms: sino_plot_rays, sino_geom_plot!
 using ImageGeoms: ImageGeom
 using Test: @test, @testset, @inferred
 
 
-function _test_sino_plot_rays(geo ; nb::Int = 60, na::Int = 40)
-    d, orbit = 2, 180
-#   d, orbit = 2mm, 180.0°
-    sg = geo(; d, orbit, nb, na)
+function _test_sino_plot_rays(geo ; nb::Int = 60, na::Int = 20, d=2mm)
+    orbit = 180.0 #°
+    arg = geo() isa SinoFan ? (; dod=120mm, dsd=300mm, d=2d) : (;)
+    sg = geo(; d, orbit, nb, na, arg...)
     p1 = sino_plot_rays(sg)
     @test p1 isa Plot
 
     plot()
-    p2 = sino_geom_plot!(sg)
+    ig = ImageGeom( ; dims = (64, 62), deltas = (1,1) .* d)
+    p2 = sino_geom_plot!(sg, ig)
     @test p2 isa Plot
 
-    ig = ImageGeom()
-    p3 = sino_geom_plot!(sg; ig)
-    @test p2 isa Plot
-
-    plot(p1, p2, p3)
-
-    true
+    return plot(p1, p2, layout = (2,1))
 end
 
 
 @testset "sino_plot" begin
 #   sg = SinoFan(Val(:ge1))
     sg_list = (SinoPar, SinoFanArc, SinoFanFlat, SinoMoj)
-    for geo in sg_list
-        @test _test_sino_plot_rays(geo)
-        gui()
-#       prompt()
+    p = Array{Any}(undef, length(sg_list))
+    for (i, geo) in enumerate(sg_list)
+        p[i] = _test_sino_plot_rays(geo)
+        @test p[i] isa Plot
     end
+    plot(p..., layout = (1,4))
+    gui()
 end
