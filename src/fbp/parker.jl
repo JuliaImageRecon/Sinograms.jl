5e86b1b6e11bcafd7175283cfc17caa66232cfe4
Jeff Fessler <JeffFessler@users.noreply.github.com>
2023-01-02 11:38:07 -0500

    Fix β₀ < 0 issue (#50)
    * Fix beta<0 issue
    
    * v0.4.1
    
    * Use minimum

--- src/fbp/parker.jl
+++ src/fbp/parker.jl
@@ -82,7 +82,8 @@ function parker_weight_fan_short(
         @warn("orbit $orbit exceeds short scan $orbit_short by %g views")
     #   (orbit - orbit_short) / rad2deg(ar[begin+1] - ar[begin]))
 
-    bet = ar .- ar[begin] # trick: force 0 start, so this ignores orbit_start!
+    bet = ar .- minimum(ar) # trick: force 0 start, so this ignores orbit_start!
+    bet = max.(bet, zero(eltype(bet))) # fix for issue #49
     (gg, bb) = ndgrid(gam, bet)
 
     fun = (x) -> sin(π/2 * x)^2 # smooth out [0,1] ramp
