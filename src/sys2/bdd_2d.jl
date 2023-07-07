0f2fa571f33267802dd665b93961e26bd4bb2de6
Jeff Fessler <JeffFessler@users.noreply.github.com>
2023-07-07 15:35:48 -0600

    Update branchless DD docs (#57)
    * Update bdd docs
    
    * faster bdd test
    
    * Rename _bdd
    
    * clean doc
    
    * rm @time
    
    * fix docs comment
    
    * fix docs

--- src/sys2/bdd_2d.jl
+++ src/sys2/bdd_2d.jl
@@ -2,23 +2,26 @@
 ## Reference: Rodrigo de Barros Vimieiro
 
 ## 2-D Branchless Distance Driven Code
-using LazyGrids: ndgrid 
-using Interpolations: linear_interpolation, Flat 
+using LazyGrids: ndgrid
+using Interpolations: linear_interpolation, Flat
+
+export project_bdd, backproject_bdd
+
 
 ## Projection
 """
-    projection(phantom::AbstractMatrix{<:T}, geo)
+    project_bdd(phantom::AbstractMatrix{<:T}, geo)
 Generates a sinogram using the forward projection algorithm,
-for a phantom image and a tuple of geometry definitions 
+for a phantom image and a tuple of geometry definitions
 """
-function projection(phantom::AbstractMatrix{<:T}, geo ; draw::Bool = false) where {T <: Number}
+function project_bdd(phantom::AbstractMatrix{<:T}, geo ; draw::Bool = false) where {T <: Number}
 
     DSD = geo.DSD
-    DS0 = geo.DS0      
-    pSize = geo.pSize    
-    dSize = geo.dSize    
-    nPix = geo.nPix      
-    nDet = geo.nDet     
+    DS0 = geo.DS0
+    pSize = geo.pSize
+    dSize = geo.dSize
+    nPix = geo.nPix
+    nDet = geo.nDet
     angle = geo.angle # vector of projection view angles in radians
 
     # Detector boundaries
@@ -35,9 +38,9 @@ function projection(phantom::AbstractMatrix{<:T}, geo ; draw::Bool = false) wher
 
     # For each projection
     for proj in 1:length(angle)
-        
+
         beta = angle[proj] # angle from x-ray beam to y-axis
-        
+
         # Tube rotation
         rtubeX = -DS0*sin(beta)
         rtubeY = DS0*cos(beta)
@@ -52,7 +55,7 @@ function projection(phantom::AbstractMatrix{<:T}, geo ; draw::Bool = false) wher
         else
             axisCase = true # map on x axis
         end
-        
+
         # Mapping boundaries onto a common axis
         if (axisCase)
             detm = map2x.(rtubeX,rtubeY,rdetX,rdetY)
@@ -66,38 +69,38 @@ function projection(phantom::AbstractMatrix{<:T}, geo ; draw::Bool = false) wher
 
         # scaling factor calculation
         detSize = diff(detm, dims = 1)
-        L = zeros(1,nDet) 
+        L = zeros(1,nDet)
 
-        if (axisCase) 
-            for n = 1:nDet 
+        if (axisCase)
+            for n in 1:nDet
                 det_mid = (detm[n] + detm[n+1])/2
                 theta = atan(abs(rtubeX-det_mid)/abs(rtubeY)) # theta is the angle btw the ray to det_mid and y-axis
                 L[n] = abs(pSize./(detSize[n].*cos(theta)))
             end
         else
-            for n = 1:nDet
+            for n in 1:nDet
                 det_mid = (detm[n] + detm[n+1])/2
                 theta = acot(abs(rtubeY-det_mid)/abs(rtubeX))
                 L[n] = abs(pSize./(detSize[n].*sin(theta)))
-            end 
-        end     
-        
+            end
+        end
+
         sinoTmp = zeros(1, nDet) # one "row" of sinogram
 
         # For each image row
         for row in 1:nPix
-            
-            rowm = pixm[row,:] # mapped row from pixel mapped         
+
+            rowm = pixm[row,:] # mapped row from pixel mapped
             pixSize = diff(rowm, dims = 1)
 
             Ppj = integrate1D(img[row,:], pixSize)
-            
+
             interp_func = linear_interpolation(rowm, Ppj, extrapolation_bc=Flat())
             Pdk = interp_func.(detm)
 
-            sinoTmp = sinoTmp + abs.(diff(Pdk, dims = 1)');              
-            
-        end # Row loop 
+            sinoTmp = sinoTmp + abs.(diff(Pdk, dims = 1)')
+
+        end # Row loop
         sinogram[proj,:] = sinoTmp .* L
 
     end # Projection loop
@@ -107,22 +110,21 @@ function projection(phantom::AbstractMatrix{<:T}, geo ; draw::Bool = false) wher
 end # endfunc
 
 
-## Backprojection 
+## Backprojection
 """
-    backprojection(sinogram::AbstractMatrix{<:T}, geo)
+    backproject_bdd(sinogram::AbstractMatrix{<:T}, geo)
 Generates a reconstructed image using the back projection algorithm,
-for a sinogram and a tuple of geometry definitions 
+for a sinogram and a tuple of geometry definitions
 """
-function backprojection(sinogram::AbstractMatrix{<:T},geo ; draw::Bool = false) where {T <: Number}
+function backproject_bdd(sinogram::AbstractMatrix{<:T},geo ; draw::Bool = false) where {T <: Number}
 
-    DSD = geo.DSD      
-    DS0 = geo.DS0      
-    pSize = geo.pSize    
-    dSize = geo.dSize    
-    nPix = geo.nPix       
-    nDet = geo.nDet     
-    angle = geo.angle 
-
+    DSD = geo.DSD
+    DS0 = geo.DS0
+    pSize = geo.pSize
+    dSize = geo.dSize
+    nPix = geo.nPix
+    nDet = geo.nDet
+    angle = geo.angle
 
     # Detector boundaries
     detX = (-(nDet/2):(nDet/2)) .* dSize
@@ -139,7 +141,7 @@ function backprojection(sinogram::AbstractMatrix{<:T},geo ; draw::Bool = false) 
 
     # For each projection
     for proj in 1:length(angle)
-        
+
         beta = angle[proj]
 
         # Tube rotation
@@ -156,7 +158,7 @@ function backprojection(sinogram::AbstractMatrix{<:T},geo ; draw::Bool = false) 
         else
             axisCase = true # map on x axis
         end
-        
+
         # Mapping boundaries onto a common axis
         if (axisCase)
             detm = map2x.(rtubeX,rtubeY,rdetX,rdetY)
@@ -165,31 +167,31 @@ function backprojection(sinogram::AbstractMatrix{<:T},geo ; draw::Bool = false) 
             detm = map2y.(rtubeX,rtubeY,rdetX,rdetY)
             pixm = reverse(map2y.(rtubeX,rtubeY,pixelX,pixelY)', dims = 2)
         end
-        
-        # scaling factor calculation  
-        detSize = diff(detm, dims = 1)
-        L = zeros(nDet) 
 
-        if (axisCase) 
-            for n = 1:nDet 
+        # scaling factor calculation
+        detSize = diff(detm, dims = 1)
+        L = zeros(nDet)
+
+        if (axisCase)
+            for n in 1:nDet
                 det_mid = (detm[n] + detm[n+1])/2
                 theta = atan(abs(rtubeX-det_mid)/abs(rtubeY)) # theta is the angle btw the ray to det_mid and y-axis
                 L[n] = abs(pSize./(detSize[n].*cos(theta)))
             end
         else
-            for n = 1:nDet
+            for n in 1:nDet
                 det_mid = (detm[n] + detm[n+1])/2
                 theta = acot(abs(rtubeY-det_mid)/abs(rtubeX))
                 L[n] = abs(pSize./(detSize[n].*sin(theta)))
-            end 
-        end 
+            end
+        end
 
         # For each row
         for row in 1:nPix
-            
+
             rowm = pixm[row,:] # Get first mapped row from image --> changed to column vec
-            detSize = diff(detm, dims = 1)      
-            
+            detSize = diff(detm, dims = 1)
+
             #detm = vec(detm)
             Ppj = integrate1D(sinogram[proj,:] .* L, detSize)
 
@@ -199,20 +201,20 @@ function backprojection(sinogram::AbstractMatrix{<:T},geo ; draw::Bool = false) 
                 interp_func = linear_interpolation(detm, vec(Ppj), extrapolation_bc=Flat())
             end
             Pdk = interp_func.(vec(rowm))
-            
+
             reconImgTmp[row,:] = abs.(diff(Pdk, dims = 1)')
-            
-        end # Row loop 
-        
+
+        end # Row loop
+
         if (axisCase)
-            reconImg = reconImg + reconImgTmp 
+            reconImg = reconImg + reconImgTmp
         else
             reconImg = reconImg + reverse(reconImgTmp, dims = 2)'
-        end 
-           
+        end
+
     end # Projection loop
-    
-    # reconImg = reconImg / length(angle) 
+
+    # reconImg = reconImg / length(angle)
     return reconImg
 
 end # endfunc
@@ -222,17 +224,17 @@ end # endfunc
 """
     integrate1D(p_v::Vector, pixelSize)
 Calculates the integral image set,
-for a given column vector and a pixel size 
+for a given column vector and a pixel size
 """
 function integrate1D(p_v::Vector,pixelSize::Vector)
     n_pixel = length(p_v)
-    
+
     P_x = 0
     Ppj = zeros(n_pixel+1)
-    
+
     for pj in 1:n_pixel
        P_x += p_v[pj] * pixelSize[pj]
-       
+
        Ppj[pj+1] = P_x
     end
     return Ppj
--- test/sys2/bdd_2d.jl
+++ test/sys2/bdd_2d.jl
@@ -1,69 +1,55 @@
-using Sinograms: projection, backprojection, SinoFanFlat, rays, plan_fbp, fbp, Window, Hamming
+# test/bdd_2d.jl
+
+using Sinograms: project_bdd, backproject_bdd
+using Sinograms: SinoFanFlat, rays, plan_fbp, fbp, Window, Hamming
+import Sinograms # downsample, _ar, _dso
+using ImageGeoms: ImageGeom, fovs, MaskCircle
+import ImageGeoms # downsample
 using ImagePhantoms: SheppLogan, shepp_logan, SheppLoganToft, radon, phantom
 using Test: @test, @testset, @inferred
 using LinearAlgebra: dot
-
-using ImageGeoms: ImageGeom, fovs, MaskCircle
-# using MIRTjim: jim, prompt
 using Unitful: mm
 
 @testset "bdd_2d" begin
     # Define geometry
-    deg = 1
-    geo = (DSD = 949mm, DS0 = 541mm, pSize = 1mm, dSize = 1.0239mm,
-        nPix = 512, nDet = 888, angle = deg2rad.(0:deg:360-deg))
+    down = 4 # faster test
+    ig = ImageGeom(MaskCircle(); dims=(512,512), deltas = (1mm,1mm))
+    rg = SinoFanFlat( ; nb = 910, d = 1.0239mm, na = 360, dsd = 949mm, dod = 408mm)
+    ig = ImageGeoms.downsample(ig, down)
+    rg = Sinograms.downsample(rg, down)
 
-    ig = ImageGeom(MaskCircle(); dims=(512,512), deltas = (1mm,1mm) )
-    rg = SinoFanFlat( ; nb = 888, d = 1.0239mm, na = 360, dsd = 949mm, dod = 408mm)
+    geo = (DSD = rg.dsd, DS0 = Sinograms._dso(rg), pSize = ig.deltas[1],
+        dSize = rg.d, nPix = ig.dims[1], nDet = rg.nb, angle = Sinograms._ar(rg))
 
     # Ellipse parameters for Shepp-Logan phantom
     μ = 0.01 / mm # typical linear attenuation coefficient
     ob = shepp_logan(SheppLoganToft(); fovs = fovs(ig), u = (1, 1, μ))
 
     testimage = phantom(axes(ig)..., ob)
-#   jim(reverse(testimage, dims=2)) # show the phantom image
 
-    sinogramB = projection(reverse(rot180(testimage'), dims=2),geo)
+    # forward projector
+    sinogramB = project_bdd(reverse(rot180(testimage'), dims=2), geo)
     sinogram = sinogramB / 1mm # include units
-    imageB = backprojection(sinogram,geo)
 
-    # Check dimensions
+    # check dimensions
     @test sinogramB isa Matrix
     @test size(sinogramB) == (length(geo.angle), geo.nDet)
-    @test imageB isa Matrix
-    @test size(imageB) == (geo.nPix, geo.nPix)
 
     # Check radon test
     sinogramR = radon(rays(rg), ob)
-#=
-    p1 = jim(1:geo.nDet, 1:length(geo.angle), sinogramB'; title="bdd_2d sinogram", aspect_ratio = :auto, xlabel="r (mm)", ylabel="ϕ")
-    p2 = jim(axes(rg), sinogramR; title="Radon Test", xlabel="r", ylabel="ϕ")
-    p3 = jim(axes(rg), sinogramR - sinogramB', title="Error image", aspect_ratio = :auto, xlabel="r", ylabel="ϕ")
-    jim(p1,p2,p3)
-=#
 
-    # Image reconstruction via FBP
-    plan = plan_fbp(rg, ig; window = Window(Hamming(), 1.0))
-    fbp_image_b = fbp(plan, sinogramB')
-    fbp_image_r = fbp(plan, sinogramR)
-    clim = (0.04, 1.1) .* μ
-#=
-    r1 = jim(axes(ig), fbp_image_b, "FBP image with bdd_2d"; clim)
-    r2 = jim(axes(ig), fbp_image_r, "FBP image with radon"; clim)
-    r3 = jim(axes(ig), fbp_image_b - fbp_image_r, "Error image")
-    jim(r1,r2,r3)
+    # back projector
+    imageB = backproject_bdd(sinogram, geo)
+    @test imageB isa Matrix
+    @test size(imageB) == (geo.nPix, geo.nPix)
 
-    # Backprojection of bdd_2d
-    jim(1:geo.nPix, 1:geo.nPix, imageB'; title="bdd_2d backprojection", aspect_ratio = :auto, xlabel="mm", ylabel="mm")
-=#
-
-    #check adjoint
+    # adjoint
     x = rand(geo.nPix, geo.nPix) / 1mm
-    sinoX = projection(x, geo)
+    sinoX = project_bdd(x, geo)
 
     y = rand(length(geo.angle), geo.nDet) / 1mm
-    imY = backprojection(y, geo)
+    imY = backproject_bdd(y, geo)
 
     @show (dot(sinoX,y), dot(x, imY))
-    @test isapprox(dot(sinoX,y), dot(x, imY); rtol = 3e-3)
+    @test isapprox(dot(sinoX,y), dot(x, imY); rtol = 2e-2)
 end
