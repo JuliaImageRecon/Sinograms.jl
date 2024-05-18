86c16d7eccc772f3616863993b7e11d17851a511
Jeff Fessler <fessler@umich.edu>
2024-05-18 11:27:26 -0400

    Test codecov

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,11 @@ using Test: @test, @test_throws, @testset, detect_ambiguities
 
 # test extension stubs before loading the extension
 @testset "exts" begin
-    @test_throws String sino_plot_rays()
-#   @test_throws String sino_geom_plot!()
+# todo
+#   @test_throws String sino_plot_rays()
+    @test_throws String sino_geom_plot!()
     @test_throws String ct_geom_plot2!()
-    @test_throws String ct_geom_plot3() # todo
+    @test_throws String ct_geom_plot3()
 end
 
 include("units.jl")
